7a109e1f2cadf0dd6dfcc355bf37f307d7c99ba5
Akhil Satish <akhilsatish2008@gmail.com>
2022-05-26 19:03:15 -0700

    clientside js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let game = document.addEventListener('click',playRound);
+
+async function playRound(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+}
